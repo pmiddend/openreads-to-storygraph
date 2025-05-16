7362c8899f561789a22432c7ce3e5e7d1d458103
Philipp Middendorf <philipp.middendorf@desy.de>
2025-05-16 10:39:55 +0200

    💚 CI: add git version dependencies

--- app/Main.hs
+++ app/Main.hs
@@ -1,1 +1,8 @@
+module Main where
+
+import Data.Version (showVersion)
+import Paths_openreads_to_storygraph (version)
+
+openreadsToStorygraphVersion = showVersion version -- VERSIONSTRING
+
 main = putStrLn "hello world"
